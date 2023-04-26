764063e68cefc3f24e646c8dd721a35ac0d2539d
Javier Garrido <javier.garrido@catena.cl>
2023-04-25 20:23:45 -0400

    Co-authored-by: Caterina Vivero <Caty-vt@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-/*let numero = parseInt(prompt("ingrese un numero menor a 100"));
-let sumatoria = 0;
-while(!Number.isInteger(numero)||numero>100||numero<1){
-  numero = parseInt(prompt("ingrese un numero menor a 100"));
-}
-for(let i=1; i<=numero;i++){
-  sumatoria+=i
-}
-alert(sumatoria)*/
+/*
+
 let primo = true 
 let calcularPrimo = function(){
   let numero = parseInt(prompt("ingrese un numero"));
@@ -28,3 +21,59 @@ let calcularPrimo = function(){
   
 }
 calcularPrimo();
+
+let numero = parseInt(prompt("ingrese un numero entero menor que 100"))
+let numeroaMostar = 0;
+while(!Number.isInteger(numero)||numero>100||numero<1){
+  numero = parseInt(prompt("ingrese un numero menor a 100"));
+}
+numeroaMostar = numero
+console.log(numeroaMostar)
+for(let i = 0; i<numero-1;i++){
+  numeroaMostar = numeroaMostar -1
+console.log(numeroaMostar)
+}
+
+function cuentaRegresiva() {
+  let n = parseInt(prompt("Ingrese un número entero menor que 100"));
+  while(!Number.isInteger(n)||n>100||n<1){
+    numero = parseInt(prompt("ingrese un numero menor a 100"));
+  }
+  
+  for (let i = n; i >= 1; i--) {
+    console.log(i);
+  }
+}
+cuentaRegresiva();
+
+
+
+
+
+function funcionSumatoriaPares(){
+  let numero = parseInt(prompt("ingrese un numero mayor que 10 y menor que 1000"));
+let sumatoria = 0;
+while(!Number.isInteger(numero)||numero>1000||numero<=10){
+  numero = parseInt(prompt("ingrese un numero mayor que 10 y menor que 1000"));
+}
+for(let i=11; i<=numero;i++){
+  if((i%2)===0){
+    sumatoria+=i
+  }
+}
+alert('la suma de todos los pares desde 11 hasta el numero '+numero+' es de '+sumatoria)
+}
+funcionSumatoriaPares()
+*/
+//declare lo necesario para realizar un café y genere la rutina para realizarlo.
+
+function multiplicar(){
+  let numero = parseInt(prompt("ingrese un numero"));
+  while(!Number.isInteger(numero)){
+    numero = parseInt(prompt("ingrese un numero"));
+  };
+for(let i = 1; i<=12; i++){
+  console.log( numero +" X "+i+" = "+numero*i);
+};
+};
+multiplicar();
